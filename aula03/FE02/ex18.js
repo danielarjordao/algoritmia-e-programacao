8f2579ce450f960a1d198d9c64e8519c8f9cc940
danielarjordao <danielarjordao@hotmail.com>
2025-12-15 17:06:15 +0000

    aula03

--- aula03/FE02/ex18.js
+++ aula03/FE02/ex18.js
@@ -7,4 +7,17 @@
 // mostrado no ecrã o recibo de salário do funcionário após a aplicação da
 // sobretaxa.
 
-// TO_DO
+const prompt = require("prompt-sync")();
+
+const SALARIO_MINIMO = 870;
+const salarioFuncionario = Number(prompt("Digite o salário do funcionário: "));
+
+if (salarioFuncionario <= SALARIO_MINIMO) {
+	console.log("O funcionário aufere o salário mínimo nacional. Não há sobretaxa a aplicar.");
+	console.log(`Recibo de salário: €${salarioFuncionario.toFixed(2)}`);
+} else {
+	const valorSobretaxa = (salarioFuncionario - SALARIO_MINIMO) * 0.035;
+	const salarioFinal = salarioFuncionario - valorSobretaxa;
+
+	console.log(`Recibo de salário após aplicação da sobretaxa: €${salarioFinal.toFixed(2)}`);
+}
